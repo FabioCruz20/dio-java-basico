48ec9eac19ddf5ba2c3a6f31c7bdcc06020abac7
FabioCruz20 <fabiovict95@gmail.com>
2025-03-21 17:12:36 -0300

    refactor: remove unused scanner variable

--- inheritance-and-polymorphism/exercise-1/src/App.java
+++ inheritance-and-polymorphism/exercise-1/src/App.java
@@ -1,8 +1,4 @@
-import java.util.Scanner;
-
 public class App {
-
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
         
